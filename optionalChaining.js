b94bf0a09fb67dfbf57c813cb0fc5f35a6dc15a7
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-03 02:21:16 +0600

    Object OptionalChaing

---
+++ optionalChaining.js
@@ -0,0 +1,25 @@
+const petOwner = {
+  name: "Sahabul Islam Sifat",
+  contact: {
+    phone: "01632165523",
+    email: "sahabulislamsifat",
+    address: "Dhaka,Bangladesh",
+  },
+  pet: {
+    name: "Black Sheep",
+    info: {
+      color: "black",
+      weight: "5.4 kg",
+    },
+  },
+  food: ["Orange", "Mango", "JackFruit", "Apple"],
+};
+
+// console.log(petOwner.name);
+// console.log(petOwner.contact.phone);
+// console.log(petOwner.pet.info.color);
+// console.log(petOwner.pet.age.weight);
+// console.log(petOwner.pet.age?.weight);
+// console.log(petOwner.food[0]);
+// console.log(petOwner.food[3]);
+console.log(petOwner.food[5]);
