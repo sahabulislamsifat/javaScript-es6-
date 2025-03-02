dd4312dcfdd36ece0723c47165a01e6c99877ba3
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-03 00:31:50 +0600

    js normal and arrow function

---
+++ arrow.js
@@ -0,0 +1,51 @@
+// Comperision Normal and Arrow Function
+
+// function info() {
+//   return "ki Chai....?";
+// }
+
+// const info = () => {
+//   return "ki chai......?";
+// };
+
+// const info = () => "ki chai....?";
+
+// const result = info();
+// console.log(result);
+
+// function double(x) {
+//   return x * 2;
+// }
+
+// const double = (x) => x * 2;
+// const double = (x) => x * 2;
+
+// console.log(double(4));
+// console.log(double(8));
+
+// const plus = (x, y) => {
+//   const total = x + y;
+//   return total;
+// };
+
+// const plus = (x, y) => x + y;
+
+// console.log(plus(4, 5));
+// console.log(plus(10, 5));
+
+// const minus = (x, y) => {
+//   const biyog = x - y;
+//   return biyog;
+// };
+
+// const minus = (x, y = 0) => {
+//   const biyog = x - y;
+//   return biyog;
+// };
+
+// const minus = (x, y = 5) => {
+//   const biyog = x - y;
+//   return biyog;
+// };
+
+// console.log(minus(20));
