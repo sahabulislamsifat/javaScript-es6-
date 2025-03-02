99efbb84a55599e1a6305fd5fac5602244f1c413
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-03 03:02:59 +0600

    explore javaScript map function

---
+++ map.js
@@ -0,0 +1,17 @@
+const array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// array.map((item, index, wholeArray) => {
+//   //   console.log(item);
+//   //   console.log(item, index);
+//   //   console.log(item, index, wholeArray);
+// });
+
+// const result = array.forEach((a) => a);
+// const result = array.map((a) => a);
+
+// const result = array.map((item) => {
+//   const plus = item + 5;
+//   return plus;
+// });
+
+// console.log(result);
