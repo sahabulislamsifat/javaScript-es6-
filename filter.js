239f1fc76385875b967d734276d6ae108c587127
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-03 03:22:49 +0600

    JavaScript Filter Mathod

---
+++ filter.js
@@ -0,0 +1,7 @@
+const numbers = [18, 48, 53, 41, 38, 46, 80, 25];
+
+// const result = numbers.filter((item) => item > 50);
+const result = numbers.filter((item) => item > 200);
+
+console.log(result);
+console.log(!!result);
