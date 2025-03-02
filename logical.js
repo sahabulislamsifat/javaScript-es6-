1847d4f01d9a92521447df7043ee34c47f78290a
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-03 00:51:14 +0600

    Logical and Ternary Operator

---
+++ logical.js
@@ -0,0 +1,33 @@
+// Logical Operator (&&, ||, !) ... and, or, not
+
+// const x = 5;
+// console.log(x);
+// console.log(!x);
+// console.log(!!x);
+
+// const money = true;
+// const money = false;
+// const money = 0;
+// const money = '';
+// const money = '50';
+// const money = 50;
+
+// && and
+// left && Right
+
+// console.log(money && "Varsity Jabo...");
+
+// const bus = "Bus Available...";
+// const bus = "";
+// const rikshwa = "Rikshwa Available";
+
+// const result = bus || rikshwa;
+// console.log(result);
+
+// TERNARY OPERATOR
+// const x = 5;
+// const x = 50;
+
+// Statement ? if true : false
+
+// console.log(x === 5 ? "its five" : "not five");
